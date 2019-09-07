3562248de1ee24dd4973c8b1065f57b4f08ad7fc
Arianna Kosten <arianna.kosten@gmail.com>
2019-09-07 03:34:27 +0000

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,14 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
-
+  
+  def new
+    
+  end
+  
+  def create 
+    Student.create(first_name: params[:student][:first_name], last_name: params[:student][:last_name])
+    redirect_to students_path 
+    #Print out the student value to the view template page
+  end 
 end
